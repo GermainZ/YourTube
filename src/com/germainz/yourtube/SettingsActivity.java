3ce353d4eba0da12d77d913c8a41141c81423e22
GermainZ <germanosz@gmail.com>
2014-06-12 15:27:25 +0300

    Add preferences and support for latest YouTube version; version bump to 1.1.1

---
+++ src/com/germainz/yourtube/SettingsActivity.java
@@ -0,0 +1,76 @@
+package com.germainz.yourtube;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+
+public class SettingsActivity extends Activity {
+
+    private static final String PANE_PLAYLIST = "0";
+    private static final String PANE_SUBSCRIPTION = "1";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (savedInstanceState == null)
+            getFragmentManager().beginTransaction().replace(android.R.id.content, new PrefsFragment()).commit();
+
+    }
+
+    public static class PrefsFragment extends PreferenceFragment {
+        @SuppressWarnings("deprecation")
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            getPreferenceManager().setSharedPreferencesMode(MODE_WORLD_READABLE);
+            addPreferencesFromResource(R.xml.preferences);
+
+            ListPreference defaultPanePref = (ListPreference) findPreference("pref_default_pane");
+            final EditTextPreference playlistPref = (EditTextPreference) findPreference("pref_playlist");
+            final EditTextPreference subscriptionPref = (EditTextPreference) findPreference("pref_subscription");
+            Preference showAppIconPref = findPreference("pref_show_app_icon");
+
+            if (defaultPanePref.getValue().equals(PANE_PLAYLIST))
+                playlistPref.setEnabled(true);
+            else if (defaultPanePref.getValue().equals(PANE_SUBSCRIPTION))
+                subscriptionPref.setEnabled(true);
+
+            defaultPanePref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object o) {
+                    if (o.equals(PANE_PLAYLIST)) {
+                        playlistPref.setEnabled(true);
+                        subscriptionPref.setEnabled(false);
+                    } else if (o.equals(PANE_SUBSCRIPTION)) {
+                        subscriptionPref.setEnabled(true);
+                        playlistPref.setEnabled(false);
+                    } else {
+                        subscriptionPref.setEnabled(false);
+                        playlistPref.setEnabled(false);
+                    }
+                    return true;
+                }
+            });
+
+            showAppIconPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    Activity act = getActivity();
+                    PackageManager p = act.getPackageManager();
+                    int state = (Boolean) newValue ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED : PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+                    final ComponentName alias = new ComponentName(getActivity(), "com.germainz.yourtube.SettingsActivity-Alias");
+                    p.setComponentEnabledSetting(alias, state, PackageManager.DONT_KILL_APP);
+                    return true;
+                }
+            });
+        }
+    }
+}
--- src/com/germainz/yourtube/XposedMod.java
+++ src/com/germainz/yourtube/XposedMod.java
@@ -2,60 +2,77 @@ package com.germainz.yourtube;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
+import static de.robv.android.xposed.XposedHelpers.callMethod;
 import static de.robv.android.xposed.XposedHelpers.callStaticMethod;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
+import static de.robv.android.xposed.XposedHelpers.getObjectField;
 import static de.robv.android.xposed.XposedHelpers.newInstance;
+import static de.robv.android.xposed.XposedHelpers.setObjectField;
 
 public class XposedMod implements IXposedHookLoadPackage {
+
+    private static final String PREF_DEFAULT_PANE = "pref_default_pane";
+    private static final String PREF_PLAYLIST = "pref_playlist";
+    private static final String PREF_SUBSCRIPTION = "pref_subscription";
+    private static final String DEFAULT_PANE = "FEsubscriptions";
+    private static final String PANE_PLAYLIST = "0";
+    private static final String PANE_SUBSCRIPTION = "1";
 
     @Override
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
         if (!lpparam.packageName.equals("com.google.android.youtube"))
             return;
 
+        final XSharedPreferences prefs = new XSharedPreferences("com.germainz.yourtube");
+
         findAndHookMethod("com.google.android.apps.youtube.app.GuideActivity", lpparam.classLoader,
                 "D", new XC_MethodReplacement() {
                     @Override
                     protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-                        Class paneDescriptorClass = findClass("com.google.android.apps.youtube.app.fragments.navigation.PaneDescriptor", lpparam.classLoader);
-                        Class mySubscriptionsFragment = findClass("com.google.android.apps.youtube.app.fragments.MySubscriptionsFragment", lpparam.classLoader);
-                        Class dClass = findClass("com.google.android.apps.youtube.app.fragments.navigation.d", lpparam.classLoader);
-                        Object bundle = callStaticMethod(dClass, "a", 3);
-                        Object panedescriptor = newInstance(paneDescriptorClass, mySubscriptionsFragment, bundle);
-//                        String s = "FEwhat_to_watch";
-//                        Class d = findClass("com.google.android.apps.youtube.app.fragments.navigation.d", lpparam.classLoader);
-//                        Object panedescriptor = callStaticMethod(d, "b", s, false);
-//                        Class kz = findClass("com.google.a.a.a.a.kz", lpparam.classLoader);
-//                        Object kz1 = newInstance(kz);
-//                        Class am = findClass("com.google.a.a.a.a.am", lpparam.classLoader);
-//                        setObjectField(kz1, "c", newInstance(am));
-//                        Object c = getObjectField(kz1, "c");
-//                        setObjectField(c, "b", s);
-//                        callMethod(panedescriptor, "setNavigationEndpoint", kz1);
-                        return panedescriptor;
+                        String paneString = prefs.getString(PREF_DEFAULT_PANE, DEFAULT_PANE);
+                        if (paneString.equals(PANE_PLAYLIST))
+                            paneString = "VL" + prefs.getString(PREF_PLAYLIST, "");
+                        else if (paneString.equals(PANE_SUBSCRIPTION))
+                            paneString = prefs.getString(PREF_SUBSCRIPTION, "");
+
+                        Class paneDescriptorHelper = findClass("com.google.android.apps.youtube.app.fragments.navigation.d", lpparam.classLoader);
+                        Object paneDescriptor = callStaticMethod(paneDescriptorHelper, "a", paneString, false);
+                        Class aClass1 = findClass("com.google.a.a.a.a.nn", lpparam.classLoader);
+                        Object aClass1Instance = newInstance(aClass1);
+                        Class aClass2 = findClass("com.google.a.a.a.a.bb", lpparam.classLoader);
+                        setObjectField(aClass1Instance, "d", newInstance(aClass2));
+                        Object c = getObjectField(aClass1Instance, "d");
+                        setObjectField(c, "b", paneString);
+                        callMethod(paneDescriptor, "setNavigationEndpoint", aClass1Instance);
+
+                        return paneDescriptor;
                     }
                 }
         );
 
-//        findAndHookMethod("com.google.android.apps.youtube.app.fragments.BrowseFragment", lpparam.classLoader,
-//                "a", LayoutInflater.class, ViewGroup.class, Bundle.class, new XC_MethodHook() {
-//                    @Override
-//                    protected void afterHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
-//                        XposedBridge.log("Z: " + getObjectField(param.thisObject, "Z"));
-                        // What to watch: FEwhat_to_watch
-                        // Watch Later: VLWL
-                        // Playlist: VL + <playlist_id>
-                        // get playlist id from: https://www.youtube.com/playlist?list=playlist_id
-                        // Liked videos: same as above
-                        // subscriptions: <subscription_id>
-                        // get subscriptions id from: https://www.youtube.com/channel/subscription_id
-                        // Browse channels: FEguide_builder
-//                    }
-//                }
-//        );
+/*
+        findAndHookMethod("com.google.android.apps.youtube.app.fragments.BrowseFragment", lpparam.classLoader,
+                "a", LayoutInflater.class, ViewGroup.class, Bundle.class, new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+                        XposedBridge.log("Pane: " + getObjectField(param.thisObject, "h"));
+        // What to watch: FEwhat_to_watch
+        // Subscriptions: FEsubscriptions
+        // Watch Later: VLWL
+        // Playlist: VL + <playlist_id>
+        // get playlist id from: https://www.youtube.com/playlist?list=playlist_id
+        // Liked videos: same as above
+        // subscriptions: <subscription_id>
+        // get subscriptions id from: https://www.youtube.com/channel/subscription_id
+        // Browse channels: FEguide_builder
+                    }
+                }
+        );
+*/
 
     }
 }

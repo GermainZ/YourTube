bdcabdcf0c9181ea8e8ad26ba852debb51f49f03
GermainZ <germanosz@gmail.com>
2014-10-05 03:32:54 +0300

    Add preference to select maximum default stream quality

--- src/com/germainz/yourtube/XposedMod.java
+++ src/com/germainz/yourtube/XposedMod.java
@@ -1,14 +1,19 @@
 package com.germainz.yourtube;
 
+import java.util.ArrayList;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
+import static de.robv.android.xposed.XposedHelpers.callMethod;
 import static de.robv.android.xposed.XposedHelpers.callStaticMethod;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
+import static de.robv.android.xposed.XposedHelpers.getIntField;
+import static de.robv.android.xposed.XposedHelpers.getObjectField;
 
 public class XposedMod implements IXposedHookLoadPackage {
 
@@ -16,9 +21,13 @@ public class XposedMod implements IXposedHookLoadPackage {
     private static final String PREF_PLAYLIST = "pref_playlist";
     private static final String PREF_SUBSCRIPTION = "pref_subscription";
     private static final String PREF_OVERRIDE_DEVICE_SUPPORT = "pref_override_device_support";
+    private static final String PREF_MAXIMUM_STREAM_QUALITY = "pref_maximum_stream_quality";
     private static final String DEFAULT_PANE = "FEsubscriptions";
+    private static final String DEFAULT_STREAM_QUALITY = "-2";
     private static final String PANE_PLAYLIST = "0";
     private static final String PANE_SUBSCRIPTION = "1";
+    private static boolean sNewVideo = true;
+    private static ArrayList<Integer> sStreamQualities;
 
     @Override
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
@@ -57,5 +66,46 @@ public class XposedMod implements IXposedHookLoadPackage {
         findAndHookMethod("bts", lpparam.classLoader, "B", deviceSupportHook);
         findAndHookMethod("bts", lpparam.classLoader, "y", deviceSupportHook);
         findAndHookMethod("bts", lpparam.classLoader, "z", deviceSupportHook);
+
+        findAndHookMethod("com.google.android.apps.youtube.app.fragments.PlayerFragment", lpparam.classLoader, "a",
+                boolean.class, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        sNewVideo = true;
+                    }
+                });
+
+        findAndHookMethod("cls", lpparam.classLoader, "handleFormatStreamChangeEvent", "fdy", new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                Object[] info = (Object[]) getObjectField(param.args[0], "d");
+                sStreamQualities = new ArrayList<Integer>();
+                for (Object streamQuality : info) {
+                    sStreamQualities.add(getIntField(streamQuality, "a"));
+                }
+            }
+        });
+
+        findAndHookMethod("cmc", lpparam.classLoader, "a", String[].class, int.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                if (sNewVideo) {
+                    sNewVideo = false;
+                    int maximumStreamQuality = Integer.parseInt(prefs.getString(PREF_MAXIMUM_STREAM_QUALITY,
+                            DEFAULT_STREAM_QUALITY));
+                    int quality = -2;
+                    for (int streamQuality : sStreamQualities) {
+                        if (streamQuality <= maximumStreamQuality)
+                            quality = streamQuality;
+                    }
+                    if (quality == -2)
+                        quality = sStreamQualities.get(sStreamQualities.size() - 1);
+                    else
+                        quality = sStreamQualities.indexOf(quality);
+                    param.args[1] = quality;
+                    callMethod(getObjectField(param.thisObject, "H"), "a", quality);
+                }
+            }
+        });
     }
 }
